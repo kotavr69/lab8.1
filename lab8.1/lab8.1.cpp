2a3ed54725db4f4d8f97041f43c6c2d36f507574
kotavr69 <144414802+kotavr69@users.noreply.github.com>
2023-12-07 22:44:42 +0700

    Добавьте файлы проекта.

---
+++ lab8.1/lab8.1.cpp
@@ -0,0 +1,24 @@
+﻿/*Вариант 11
+Дана строка. Подсчитать общее количество содержащихся в ней
+строчных латинских букв.
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    size_t k = 0;
+    string str;
+    cout << "Введите строчку из букв: ";
+    getline(cin, str, '\n');
+    for (int i = 0; i < str.length(); i++) {
+        char ch = str[i];
+        if ('a' <= ch && ch <= 'z') {
+            k++;
+        }
+    }
+    cout << "Количество строчных латинских букв: " << k;
+    return 0;
+}
